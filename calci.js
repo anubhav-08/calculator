e30c0277c4e50b9e1c3e1c816fd0877ed3346af1
Anubhav Tyagi <63876814+anubhav-08@users.noreply.github.com>
2020-04-30 21:55:44 +0530

    update calci.js

---
+++ calci.js
@@ -0,0 +1,124 @@
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+function updateDisplay() {
+  const display = document.querySelector('.scr');
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
+
+//keys
+const keys = document.querySelector('.keypad')
+keys.addEventListener('click', (event)=>{
+	const {target} = event;
+	if(!target.matches('button'))
+		{
+			return;
+		}
+	if(target.classList.contains('decimal'))
+	{
+		inputDecimal(target.value);
+		updateDisplay();
+		return;
+	}
+	if (target.classList.contains('operator'))
+	{
+	 	handleOperator(target.value);
+	 	updateDisplay();
+		return;
+	}
+	if (target.classList.contains('all-clear'))
+	{
+		resetCalculator();
+		updateDisplay();
+		return;
+	}
+	inputDigit(target.value);
+	updateDisplay();	
+})
+
+function inputDigit(digit) {
+	const {displayValue, waitingForSecondOperand} = calculator;
+	if(waitingForSecondOperand === true)
+	{
+		calculator.displayValue = digit;
+		calculator.waitingForSecondOperand= false;
+	}
+
+	else
+	{
+		calculator.displayValue = displayValue === '0'? digit : displayValue + digit;
+	}
+	console.log(calculator);
+}
+
+function inputDecimal(dot){
+	if (calculator.waitingForSecondOperand === true) return;
+
+	if(!calculator.displayValue.includes(dot))
+	{
+		const {displayValue} = calculator;
+		calculator.displayValue += dot;
+	} 
+}
+
+//operator
+function handleOperator(nextOperator)
+{
+	const {displayValue, firstOperand, operator} = calculator;
+	const inputValue = parseFloat(displayValue);
+	
+	if(firstOperand === null)
+	{
+		calculator.firstOperand = inputValue;
+	}
+
+	else if (operator !== '**')
+	{
+		const result = performCalculation[operator](firstOperand,inputValue);
+		calculator.displayValue = String(result);
+		calculator.firstOperand = result;
+	}
+	else if (operator === '**'){
+		const result = performSquare[operator](firstOperand);
+
+		calculator.displayValue = String(result);
+		calculator.firstOperand = result;
+		return;
+	}
+
+
+	calculator.waitingForSecondOperand  = true;
+	calculator.operator = nextOperator;
+
+}
+const performCalculation = {
+
+	'/': (firstOperand, secondOperand)=> firstOperand/secondOperand,
+	'*': (firstOperand, secondOperand)=> firstOperand*secondOperand,
+	'+': (firstOperand, secondOperand)=> firstOperand+secondOperand,
+	'-': (firstOperand, secondOperand)=> firstOperand-secondOperand,
+	
+	'x*y':(firstOperand,secondOperand)=>{
+		let sol = 1;
+		for (let i=1; i<= secondOperand; i++){
+			sol = sol*firstOperand;
+		}
+		return sol;
+	} 
+};
+//reset calculator
+const performSquare = {
+	'**':(firstOperand)=> firstOperand*firstOperand
+};
+
+function resetCalculator(){
+	calculator.displayValue = '0';
+	calculator.firstOperand = null;
+	calculator.waitingForSecondOperand = false;
+	calculator.operator = null;
+}
